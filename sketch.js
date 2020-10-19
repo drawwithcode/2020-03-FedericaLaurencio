92605f79f78dc4f8676f961595a1ff032dce14b2
FedericaLaurencio <56796977+FedericaLaurencio@users.noreply.github.com>
2020-10-19 19:53:23 +0200

    404 fix

--- sketch.js
+++ sketch.js
@@ -8,9 +8,9 @@ var analyzer
 
 
 function preload() {
-  img[0] = loadImage('/assets/images/0.png');
-  img[1] = loadImage('/assets/images/1.png');
-  img[2] = loadImage('/assets/images/2.png');
+  img[0] = loadImage("./assets/images/zero.png");
+img[1] = loadImage("./assets/images/uno.png");
+img[2] = loadImage("./assets/images/due.png");
   song[1] = loadSound("/assets/sound/rick.mp3");
   song[2] = loadSound("/assets/sound/Loser.mp3");
 
